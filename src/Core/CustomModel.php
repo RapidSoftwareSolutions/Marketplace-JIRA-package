495a12c808a2f0062986989c5c1e8b9bafc79b48
volodimir.kudriachenko <volodimir.kudriachenko@rapidapi.com>
2017-02-24 11:27:22 +0200

    Fill custom

--- src/Core/CustomModel.php
+++ src/Core/CustomModel.php
@@ -9,5 +9,64 @@ if ( ! defined( 'RAPID_IN' ) ) exit( 'No direct script access allowed' );
  */
 class CustomModel
 {
-    public static function defalut($param, &$blockCustom, $vendorUrl){}
+    public static function addAuditingRecord($param, &$blockCustom, $vendorUrl){
+        $result = $param;
+
+        return json_encode($result);
+    }
+    public static function updateSelectFieldOptions($param, &$blockCustom, $vendorUrl){
+        $result = $param;
+
+        return json_encode($result);
+    }
+    public static function createSingleIssue($param, &$blockCustom, $vendorUrl){
+        $result = $param;
+
+        return json_encode($result);
+    }
+    public static function deleteSingleIssue($param, &$blockCustom, $vendorUrl){
+        $result = $param;
+
+        return json_encode($result);
+    }
+    public static function addCommentToIssue($param, &$blockCustom, $vendorUrl){
+        $result = $param;
+
+        return json_encode($result);
+    }
+    public static function sendIssueNotify($param, &$blockCustom, $vendorUrl){
+        $result = $param;
+
+        return json_encode($result);
+    }
+    public static function createRemoteIssueLink($param, &$blockCustom, $vendorUrl){
+        $result = $param;
+
+        return json_encode($result);
+    }
+    public static function updateRemoteIssueLink($param, &$blockCustom, $vendorUrl){
+        $result = $param;
+
+        return json_encode($result);
+    }
+    public static function addIssueWatcher($param, &$blockCustom, $vendorUrl){
+        $result = $param;
+
+        return json_encode($result);
+    }
+    public static function addIssueWorklog($param, &$blockCustom, $vendorUrl){
+        $result = $param;
+
+        return json_encode($result);
+    }
+    public static function updateIssueWorklog($param, &$blockCustom, $vendorUrl){
+        $result = $param;
+
+        return json_encode($result);
+    }
+    public static function addIssueAttachment($param, &$blockCustom, $vendorUrl){
+        $result = $param;
+
+        return json_encode($result);
+    }
 }
